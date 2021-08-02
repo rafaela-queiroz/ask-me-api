c5e338091795e08c6d13fb83fc37c3cb2c8097a6
Rafaela Queiroz <rqueiroz2108@gmail.com>
2021-08-01 21:40:07 -0300

    :sparkles: Rotas de autenticação / usuários

--- modules/authentication/index.js
+++ modules/authentication/index.js
@@ -1,49 +1,55 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
 
-let { login } = require('../../mocks')
+let { login } = require("../../mocks");
+let { users } = require("../../mocks");
 
-router.get('/authentication', (req, res) => {
-  res.json({ message: 'hello from authentication' })
-})
+router.get("/authentication", (req, res) => {
+  res.json({ message: "hello from authentication" });
+});
 
-router.post('/login', (req, res) => {
+router.post("/login", (req, res) => {
   return !req.body.username
-  ? setTimeout(() => {
-    res.status(422).send([
-      {
-        "field": "username",
-        "index": 0,
-        "message": "Preencha o nome de usuário"
-      }
-    ]) }, 2000) //simula um delay de processamento do server
-  : setTimeout(() => { res.json(login) }, 1000)
-})
+    ? setTimeout(() => {
+        res.status(422).send([
+          {
+            field: "username",
+            index: 0,
+            message: "Preencha o nome de usuário",
+          },
+        ]);
+      }, 2000)
+    : setTimeout(() => {
+        let user = login.find((user) => user.username === req.body.username);
 
-/*
+        user
+          ? res.json(user)
+          : res.status(404).send({ message: "Usuário não encontrado!" });
+      }, 1000);
+});
 
-POST /login
-input:
-{
-  username: '',
-  password: ''
-}
+router.get("/users", (req, res) => {
+  return setTimeout(() => {
+    res.json(users);
+  });
+});
 
-output:
-{
-  id: 0,
-  name: ''
-}
+router.get("/users/:id", (req, res) => {
+  return !req.params.id
+    ? setTimeout(() => {
+        res.status(400).send({
+          field: "id",
+          index: 0,
+          message: "Campo ID é obrigatório",
+        });
+      })
+    : setTimeout(() => {
+        let user = users.find((user) => user.id === Number(req.params.id));
 
-GET /users/:id
-output:
-{
-  name: '',
-  photo: '', (se tiver como),
-  id: 0,
-  username: ''
-}
+        user
+          ? res.json(user)
+          : res.status(404).send({ message: "Usuário não encontrado!" });
+      });
+});
 
-*/
-
-module.exports = router
+module.exports = router;
